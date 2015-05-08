cfe0bd3fba6fabeed364229d533102888056b5cd
Denis Nazarov <zvezda@gmail.com>
2015-05-08 09:34:02 -0400

    Finish chapter 1 tracer.

--- chapter1/chat/room.go
+++ chapter1/chat/room.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/denisnazarov/goblueprints/chapter1/trace"
 	"github.com/gorilla/websocket"
 )
 
@@ -20,6 +21,9 @@ type room struct {
 
 	// clients hold all current clients in this room
 	clients map[*client]bool
+	// tracer will receive trace information of activity
+	// in the room.
+	tracer trace.Tracer
 }
 
 // newRoom makes a new room that is ready to go.
@@ -29,6 +33,7 @@ func newRoom() *room {
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
+		tracer:  trace.Off(),
 	}
 }
 
@@ -38,20 +43,25 @@ func (r *room) run() {
 		case client := <-r.join:
 			// joining
 			r.clients[client] = true
+			r.tracer.Trace("New client joined")
 		case client := <-r.leave:
 			//leaving
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("Client left")
 		case msg := <-r.forward:
+			r.tracer.Trace("Message received: ", string(msg))
 			// forward message to all clients
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					//send the message
+					r.tracer.Trace(" -- sent to client")
 				default:
 					//failed to send
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace(" -- failed to send, cleaned up client")
 				}
 			}
 		}
